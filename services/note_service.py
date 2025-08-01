04c4d114f7e7b72c6a284da62cdaa1c1b8e0caf0
Diep <diepani303@gmail.com>
2025-08-01 17:33:56 +0700

    All note, Detail note and Delete note

--- services/note_service.py
+++ services/note_service.py
@@ -27,3 +27,44 @@ def createNote(data):
             "success": False,
             "message": "Error DB"
         })
+
+def getAllNote(data):
+    try:
+        userId = data['userId']
+
+        notes = Note.query.filter(Note.userId == userId).all()
+        return jsonify([note.to_dict() for note in notes])
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({
+            "message": str(e),
+            "success": False,
+        }), 400
+
+def editNote(data):
+    try:
+        id = data['id']
+        header = data['header']
+        content = data['content']
+        color = data['color']
+        userId = data['userId']
+        backgroundId = data['backgroundId']
+
+        note = Note.query.filter(Note.id == id).first()
+        note.title = header
+        note.content = content
+        note.color = color
+        note.backgroundId = backgroundId
+
+        db.session.add(note)
+        db.session.commit()
+        return jsonify({
+            "success": True,
+            "message": "Note Updated"
+        })
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({
+            "message": str(e),
+            "success": False,
+        })
--- views/apiNote.py
+++ views/apiNote.py
@@ -15,3 +15,49 @@ def createNote():
     data = request.json
     return note_service.createNote(data)
 
+@noteApi.route('/note/getAll', methods=['POST', 'OPTIONS'])
+def getAllNote():
+    if request.method == 'OPTIONS':
+        return '', 200
+    data = request.json
+    return note_service.getAllNote(data)
+
+@noteApi.route('/note/<string:noteId>', methods=['GET', 'OPTIONS'])
+def getNote(noteId):
+    if request.method == 'OPTIONS':
+        return '', 200
+    note = Note.query.get(noteId)
+    if note:
+        return jsonify(note.to_dict())
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Note not found",
+            "noteId": noteId
+        })
+
+@noteApi.route('/note/edit', methods=['POST', 'OPTIONS'])
+def editNote():
+    if request.method == 'OPTIONS':
+        return '', 200
+    data = request.json
+    return note_service.editNote(data)
+
+@noteApi.route('/note/delete/<string:noteId>', methods=['DELETE', 'OPTIONS'])
+def deleteNote(noteId):
+    if request.method == 'OPTIONS':
+        return '', 200
+
+    note = Note.query.get(noteId)
+    if note:
+        db.session.delete(note)
+        db.session.commit()
+        return jsonify({
+            "success": True,
+            "message": "Note deleted",
+        })
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Note not found",
+        })
